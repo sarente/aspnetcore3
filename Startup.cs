3453ee72c4ce995580360fb6684fa0421675a897
Javad Fathi <k1.fathi33@gmail.com>
2020-06-21 21:31:07 +0300

    Add 3rdParty path for static files

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.FileProviders;//todo: by k1
+using System.IO;//todo: by k1
 
 namespace hoor
 {
@@ -47,8 +49,12 @@ namespace hoor
             app.UseStatusCodePages("text/plain", "Status code page, status code: {0}");
 
             app.UseHttpsRedirection();
-            //todo: have to learn
-            app.UseStaticFiles();
+            
+            //todo: have to learn -- add some codes
+            app.UseStaticFiles(new StaticFileOptions{
+                FileProvider=new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),"node_modules")),
+                RequestPath="/3rdParty"
+            });
 
             app.UseRouting();
 
