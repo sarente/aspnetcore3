d6b401bc3a032e1eccdec324194c3344783ee323
Javad Fathi <k1.fathi33@gmail.com>
2020-06-21 18:20:26 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using hoor.Models;
+using Newtonsoft.Json;
+
+namespace hoor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public string Contact()
+        {
+            var user= new User(){
+                Username="Keyvan",
+                Fullname="Keyvan Fathi",
+                Password="1234",
+            };
+            
+            var json=JsonConvert.SerializeObject(user,Formatting.Indented);
+
+            return json;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace hoor.Models
+{
+    public class User  {
+        public string Username { get; set; }
+
+        public string Fullname { get; set; }
+        
+        public string Password { get; set; }
+    }
+}
